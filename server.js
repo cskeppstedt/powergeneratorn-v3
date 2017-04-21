7b3f33b332ad537efa6e56f2708bce7c953133aa
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2017-04-21 02:52:36 +0200

    Read input data from S3 bucket

--- server.js
+++ server.js
@@ -1,41 +1,66 @@
+const AWS = require('aws-sdk')
+const concat = require('concat-stream')
 const express = require('express')
 const fs = require('fs')
 const randomSeed = require('random-seed')
 
-const generate = require('./src/generate')
 const build = require('./src/build')
+const generate = require('./src/generate')
 const tokenize = require('./src/tokenize')
 
-const INPUT_FILE = 'input.txt'
 const PREFIX_LENGTH = 3
-const SERVER_PORT = process.env.PORT
+const {
+  NODE_ENV,
+  PORT,
+  AWS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  S3_INPUT_FILE_BUCKET,
+  S3_INPUT_FILE_KEY,
+  INPUT_FILE
+} = process.env
 
-function buildMap () {
-  console.info('  > Reading input file:', INPUT_FILE)
+function getInputFileReadStream () {
+  // read from S3 in prod, but from local file when developing
+  if (NODE_ENV !== 'production') {
+    console.info('  > Creating input read stream from local file: ' + INPUT_FILE)
+    return fs.createReadStream(INPUT_FILE)
+  }
+
+  console.info('  > Creating input read stream from S3')
+  const s3client = new AWS.S3({
+    accessKeyId: AWS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  })
+  return s3client.getObject({
+    Bucket: S3_INPUT_FILE_BUCKET,
+    Key: S3_INPUT_FILE_KEY
+  }).createReadStream()
+}
+
+function buildMap (inputReadStream) {
+  console.info('  > Reading data from input read stream')
+
   return new Promise((resolve, reject) => {
-    fs.readFile(INPUT_FILE, 'utf8', (err, data) => {
-      if (err) {
-        return reject(err)
-      }
-
-      console.info('  > Tokenizing and building chain map.')
-      const tokens = tokenize(data)
-      const map = build(tokens, PREFIX_LENGTH)
-
-      console.info(`  > Chain map contains ${map.keys().length} unique keys.`)
+    const concatStream = concat({ encoding: 'string' }, (data) => {
+      console.info('  > Tokenizing and building chain map')
+      const map = build(tokenize(data), PREFIX_LENGTH)
+      console.info(`  > Chain map contains ${map.keys().length} unique keys`)
       resolve(map)
     })
+
+    inputReadStream.on('error', (err) => reject(err))
+    inputReadStream.pipe(concatStream)
   })
 }
 
 function startServer (map) {
-  console.info('  > Starting Express server.')
+  console.info('  > Starting Express server')
   return new Promise((resolve, reject) => {
     const app = express()
     app.get('/prophecy', (req, res) => res.send(prophecy(map, req.query.seed)))
     app.use(express.static('static'))
-    app.listen(SERVER_PORT, (...args) => {
-      console.info(`  > Express server started: http://0.0.0.0:${SERVER_PORT}`)
+    app.listen(PORT, (...args) => {
+      console.info(`  > Express server started: http://0.0.0.0:${PORT}`)
       resolve(map)
     })
   })
@@ -62,5 +87,10 @@ function prophecy (map, seed) {
 }
 
 // entry-point; build chain-map and start server
-buildMap().then(startServer)
+buildMap(getInputFileReadStream())
+  .then(startServer)
+  .catch((err) => {
+    console.error(err.toString())
+    process.exit(1)
+  })
 
